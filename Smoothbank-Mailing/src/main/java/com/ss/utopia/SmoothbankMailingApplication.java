455d8c30b3f1ec2d810523def015e04157e87545
Parker-Williams-Smoothstack <parker.williams@smoothstack.com>
2021-06-09 14:12:11 -0400

    Framework for the email notification system.
    +Added basic controller for sending emails
    +Added notifyByEmail method
    -Need to add email account for system to use when sending messages

---
+++ Smoothbank-Mailing/src/main/java/com/ss/utopia/SmoothbankMailingApplication.java
@@ -0,0 +1,13 @@
+package com.ss.utopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothbankMailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoothbankMailingApplication.class, args);
+	}
+
+}
