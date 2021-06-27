9bfef908e46525f18ff20f306fec8515723853bc
Parker-Williams-Smoothstack <parker.williams@smoothstack.com>
2021-06-27 13:43:29 -0400

    Token service tweak

---
+++ Smoothbank-Mailing/src/main/java/com/ss/sbank/service/TokenService.java
@@ -0,0 +1,19 @@
+package com.ss.sbank.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ss.sbank.dao.TokenDAO;
+import com.ss.sbank.entities.Token;
+
+@Service
+public class TokenService {
+	
+	@Autowired
+	TokenDAO tDAO;
+	
+	public Token getToken(String id) {
+		return tDAO.getById(id);
+	}
+
+}
